f0f73c87ac7355b2007270c7a503f8757e98ab46
fdanci <florin.danci96@gmail.com>
2020-12-30 10:59:09 +0200

    Subtly changes styling and add github url

--- portfolio_app/forms.py
+++ portfolio_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-    name = forms.CharField(max_length=200, required=True)
-    email = forms.EmailField(required=True)
-    subject = forms.CharField(max_length=100, required=True)
+    name = forms.CharField(max_length=200, required=True, label='Your name')
+    email = forms.EmailField(required=True, label='Your email')
+    subject = forms.CharField(max_length=100, required=True, label='Message subject')
     message = forms.CharField(widget=forms.Textarea, required=True)
